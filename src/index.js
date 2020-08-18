ae8d3fc488ff10bcc91f6f6d1bb5fc34ce245a12
Josh Barnes <JBarnes49@gmail.com>
2020-08-17 18:09:00 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 
 function main(limit) {
-    let url = `https://dog.ceo/api/breeds/image/random/${limit}`;
+    // let url = `https://dog.ceo/api/breeds/image/random/${limit}`;
+    let url = `https://dog.ceo/api/breed/${limit}/images/random`;
 
     fetch(url)
         .then(response => response.json())
@@ -15,7 +16,7 @@ function main(limit) {
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        const limit = $("#js-max-results").val();
+        const limit = $("#js-breed-results").val();
         main(limit);
     });
 }
@@ -23,8 +24,14 @@ function watchForm() {
 
 function displayResults(responseJson) {
     console.log(responseJson.message);
-    let images = responseJson.message.map(url => `<img src="${url}" class="results-img">`);
-    $('.hidden').html(images);
+    let image = `<img src="" class="results-img">`;
+    if (responseJson.message === 'Breed not found (master breed does not exist)') {
+        image = `<p>Sorry we couldn't find that breed</p>`;
+    } else {
+        // let images = responseJson.message.map(url => `<img src="${url}" class="results-img">`);
+        image = `<img src=${responseJson.message} class="results-img">`;
+    }
+    $('.hidden').html(image);
 }
 
 $(watchForm);
